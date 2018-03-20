d1c4dbdfa5af2fe0d6c10ec7ca70aa3090d173da
Julian Prasetyo <jul14n4v@gmail.com>
2018-03-20 15:41:06 +0700

    Change Code to Auth Multiple Social one Account

---
+++ app/SocialAccountsService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Laravel\Socialite\Contracts\User as ProviderUser;
+
+class SocialAccountsService
+{
+    public function findOrCreate(ProviderUser $providerUser, $provider)
+    {
+        $account = LinkedSocialAccount::where('provider_name', $provider)
+            ->where('provider_id', $providerUser->getId())
+            ->first();
+
+        if ($account) {
+            return $account->user;
+        } else {
+            if (auth()->user()) {
+                $user = User::where('id', auth()->user()->id)->first();
+            } else {
+                $user = User::where('email', $providerUser->getEmail())->first();
+            }
+
+            if (!$user) {
+                $user = User::create([
+                    'email' => $providerUser->getEmail(),
+                    'name' => $providerUser->getName(),
+                ]);
+            }
+
+            $user->accounts()->create([
+                'provider_id' => $providerUser->getId(),
+                'provider_name' => $provider,
+            ]);
+
+            return $user;
+
+        }
+    }
+}
